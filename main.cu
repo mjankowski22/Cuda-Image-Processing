2209de612806b929563c2e5b7ccf425f12c113a2
Michal Jankowski <rivivias@gmail.com>
2024-01-22 21:29:43 +0100

    Added Sobel operator cpu version

--- main.cu
+++ main.cu
@@ -43,7 +43,29 @@ __global__ void applySobelCUDA(unsigned char* img, unsigned char* output, int wi
     }
 }
 
+void applySobel(unsigned char* img, unsigned char* output, int width, int height, int channels) {
+    int gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
+    for (int y = 1; y < height - 1; ++y) {
+        for (int x = 1; x < width - 1; ++x) {
+            int sumX = 0, sumY = 0;
+            for (int ky = -1; ky <= 1; ky++) {
+                for (int kx = -1; kx <= 1; kx++) {
+                    unsigned char* pixel = img + ((y + ky) * width + (x + kx)) * channels;
+                    sumX += gx[ky + 1][kx + 1] * pixel[0];
+                    sumY += gy[ky + 1][kx + 1] * pixel[0];
+                }
+            }
+            int magnitude = (int)sqrt(sumX * sumX + sumY * sumY);
+            magnitude = magnitude > 255 ? 255 : magnitude;
+            output[(y * width + x) * channels] = (unsigned char)magnitude;
+            for (int c = 0; c < channels; c++) {
+                output[(y * width + x) * channels + c] = (unsigned char)magnitude;
+            }
+        }
+    }
+}
 
 
 int main()
